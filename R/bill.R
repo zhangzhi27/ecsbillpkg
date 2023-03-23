80749577642493bf25f6d080ca7046716d3d2b48
takewiki <hulilei@takeiwki.com.cn>
2023-03-23 14:00:53 +0000

    v0.1

---
+++ R/bill.R
@@ -0,0 +1,14 @@
+#' 更新ecs单据
+#'
+#' @return return value
+#' @export
+#'
+#' @examples
+#' ecsbill_sync()
+ecsbill_sync <- function() {
+
+  ecsbill = tsda::import('pyecsbill')
+  res = ecsbill$ecsbill_sync
+  return(res)
+
+}
